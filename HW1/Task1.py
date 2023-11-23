96259aca474afa99ceeec8f558c147fe78371bc0
AASilver88 <78360998+AASilver88@users.noreply.github.com>
2023-11-24 01:36:42 +0300

    HW1
    HW1_done

---
+++ HW1/Task1.py
@@ -0,0 +1,110 @@
+# Треугольник существует только тогда, когда сумма любых двух его сторон больше
+# третьей. Дано a, b, c — стороны предполагаемого треугольника. Требуется
+# сравнить длину каждого отрезка-стороны с суммой двух других. Если хотя бы в
+# одном случае отрезок окажется больше суммы двух других, то треугольника с
+# такими сторонами не существует. Отдельно сообщить является ли треугольник
+# разносторонним, равнобедренным или равносторонним.
+
+
+def user_input_number(side: str, lower_limit: int, upper_limit: int) -> int:
+    """Пользовательский ввод.
+    :param message: Сообщение пользователю.
+    :param lower_limit: Начальное значение ввода.
+    :param upper_limit: Конечное значение ввода.
+    :return: Введенное пользователем число.
+    """
+
+    message = f'Please enter a side "{side}" of triangle from {lower_limit}'\
+        + f' to {upper_limit} -> '
+
+    number = None
+    while number is None:
+
+        string = input(f'{message}')
+
+        if string.isdigit():
+
+            temp_number = int(string)
+
+            if lower_limit <= temp_number <= upper_limit:
+
+                number = temp_number
+
+    return number
+
+
+def triangle_check(triangle: dict) -> str:
+    """Проверка треугольника.
+    :param triangle: Стороны треугольника.    
+    :return: Результат проверки.
+    """
+
+    def check_is_triangle(triangle: dict) -> bool:
+        """Проверка что треугольник существует.
+        :param triangle: Стороны треугольника.    
+        :return: Результат проверки.
+        """
+
+        NUMBER_OF_SIDES_TRIANGLE = 3
+        number_of_sides = len(triangle.values())
+
+        if number_of_sides > NUMBER_OF_SIDES_TRIANGLE:
+
+            return False
+
+        perimeter = sum(triangle.values())
+
+        for value in triangle.values():
+
+            other_sides = perimeter - value
+
+            if value >= other_sides:
+
+                return False
+
+        return True
+
+    def triangle_type(triangle: dict) -> str:
+        """Проверка типа треугольника.
+        :param triangle: Стороны треугольника.    
+        :return: Тип треугольника.
+        """
+
+        number_of_different_sides = len(set(triangle.values()))
+
+        if number_of_different_sides == 3:
+
+            return f'Triangle scalene.'
+
+        elif number_of_different_sides == 2:
+
+            return f'Triangle isosceles.'
+
+        elif number_of_different_sides == 1:
+
+            return f'Triangle is equilateral.'
+
+    if check_is_triangle(triangle):
+
+        return triangle_type(triangle)
+
+    else:
+        return f'Such a triangle does not exist.'
+
+
+LOWER_LIMIT = 0
+UPPER_LIMIT = 1000
+
+A = 'a'
+B = 'b'
+C = 'c'
+
+triangle = {
+    A: user_input_number(A, LOWER_LIMIT, UPPER_LIMIT),
+    B: user_input_number(B, LOWER_LIMIT, UPPER_LIMIT),
+    C: user_input_number(C, LOWER_LIMIT, UPPER_LIMIT),
+}
+
+result_triangle_check = triangle_check(triangle)
+
+print(f'{result_triangle_check}')
---
+++ HW1/Task2.py
@@ -0,0 +1,62 @@
+# Напишите код, который запрашивает число и сообщает является ли оно простым
+# или составным. Используйте правило для проверки: «Число является простым,
+# если делится нацело только на единицу и на себя». Сделайте ограничение на
+# ввод отрицательных чисел и чисел больше 100 тысяч.
+
+
+def user_input_number(message: str, lower_limit: int, upper_limit: int) -> int:
+    """Пользовательский ввод.
+    :param message: Сообщение пользователю.
+    :param lower_limit: Начальное значение ввода.
+    :param upper_limit: Конечное значение ввода.
+    :return: Введенное пользователем число.
+    """
+
+    number = None
+    while number is None:
+
+        string = input(f'{message}')
+
+        if string.isdigit():
+
+            temp_number = int(string)
+
+            if lower_limit <= temp_number <= upper_limit:
+
+                number = temp_number
+
+    return number
+
+
+def prime_number_test(number: int) -> str:
+    """Проверка на простое число.
+    :param number: Число для проверки.    
+    :return: Результат проверки.
+    """
+
+    if number == 1 or number == 2:
+        return f'Prime number'
+
+    elif number > 2:
+        for i in range(2, number):
+            if (number % i) == 0:
+                return f'Prime number'
+
+        return f'Composite number'
+
+    else:
+        return f'Not prime number and not composite number'
+
+
+LOWER_LIMIT = 0
+UPPER_LIMIT = 100000
+
+message_for_user = f'Please enter a natural number from '\
+                   + f'{LOWER_LIMIT} to {UPPER_LIMIT} -> '
+
+number_from_user = user_input_number(
+    message_for_user, LOWER_LIMIT, UPPER_LIMIT)
+
+result = prime_number_test(number_from_user)
+
+print(f'{result} - {number_from_user}')
